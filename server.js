5d2558ad31abad438e7ed7640a8c52f25b8e7f38
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-08-14 19:47:08 -0600

    first push

--- server.js
+++ server.js
@@ -1,52 +1,57 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index')
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); // Import the uuid package
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-// Using middleware
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', api);
 
-// Get wildcard route to home page
-app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-// Get route for notes page
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+// Load notes from db.json
+const dbPath = path.join(__dirname, 'db/db.json');
+let db = [];
 
-// // API routes
-// app.get('/api/notes', (req, res) => {
-//   const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-//   res.json(notes);
-// });
+try {
+  db = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+} catch (error) {
+  console.error('Error reading db.json:', error);
+}
+  console.log("Getting data from db: "+db)
 
-// app.post('/api/notes', (req, res) => {
-//   const newNote = req.body;
-//   const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+// API Routes
+app.get('/api/notes', (req, res) => {
+  console.log('Received a request to fetch notes'); // Add this log
+  res.json({"title":"fasgdatfg","text":"sgfnjdhgmkfjh,k","id":"85013a37-2e72-4529-ac98-7ab38fbfd9ec"});
+});
 
-//   // Generate a unique id using uuidv4
-//   newNote.id = uuidv4();
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4(); // Generate a unique ID
+  
+  db.push(newNote);
 
-//   notes.push(newNote);
-//   fs.writeFileSync('db.json', JSON.stringify(notes));
+  fs.writeFile(dbPath, JSON.stringify(db), (err) => {
+    if (err) {
+      console.error('Error writing db.json:', err);
+      res.status(500).json({ error: 'Error saving note.' });
+    } else {
+      res.json(newNote);
+    }
+  });
+});
 
-//   res.json(newNote);
-// });
-
-app.listen(PORT, () => 
-  console.log(`App is running on http://localhost:${PORT}`)
-);
-
-// // Helper function to generate a unique ID (for simplicity, using a simple counter)
-// let noteIdCounter = 1;
-// function generateUniqueId() {
-//   return noteIdCounter++;
-// }
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
